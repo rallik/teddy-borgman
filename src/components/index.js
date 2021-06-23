8cb0b95e47f4122830cd71c9119663fed26fabd5
Reuben T Allik <reubenallik@gmail.com>
2021-06-22 23:15:37 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
+// import { default as Social }  from '/Social'
+// import { default as Contact }  from '/Contact'
+export { default as Footer } from './Footer';
+
+
---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Header, Navbar, Footer } from './index'
+// import Social from '../components/Social'
+// import Contact from '../components/Contact'
+
+
+const layout = ({ children }) => {
+    return (
+        <React.Fragment>
+            <Header />
+            <Navbar />
+            {children}
+            <Footer />
+            {/* <Social /> */}
+        </React.Fragment>
+    )
+}
+
+export default layout
